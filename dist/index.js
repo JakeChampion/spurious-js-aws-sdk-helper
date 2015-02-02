d2f1f933e959ea5577d577710c04c663d040db29
Jake Champion <jakechampion.jake2@googlemail.com>
2015-02-02 16:26:29 +0000

    build assets

--- dist/index.js
+++ dist/index.js
@@ -5,44 +5,45 @@ var url = require("url");
 var Strategy = require("./strategy");
 
 function port_config() {
-	var config = "spurious ports --json";
-	return JSON.parse(exec(config)); //err handling?
+  var config = "spurious ports --json";
+  return JSON.parse(exec(config)) //err handling?
+  ;
 }
 
 function docker_config() {
-	return {
-		"spurious-dynamo": [{
-			Host: process.env["DYNAMODB.SPURIOS.LOCALHOST_NAME"].split("/").pop(),
-			HostPort: url.parse(process.env["DYNAMODB.SPURIOUS.LOCALHOST_PORT"]).port
-		}],
-		"spurious-sqs": [{
-			Host: process.env["SQS.SPURIOS.LOCALHOST_NAME"].split("/").pop(),
-			HostPort: url.parse(process.env["SQS.SPURIOUS.LOCALHOST_PORT"]).port
-		}],
-		"spurious-s3": [{
-			Host: process.env["S3.SPURIOS.LOCALHOST_NAME"].split("/").pop(),
-			HostPort: url.parse(process.env["S3.SPURIOUS.LOCALHOST_PORT"]).port
-		}]
-	};
+  return {
+    "spurious-dynamo": [{
+      Host: process.env["DYNAMODB.SPURIOS.LOCALHOST_NAME"].split("/").pop(),
+      HostPort: url.parse(process.env["DYNAMODB.SPURIOUS.LOCALHOST_PORT"]).port
+    }],
+    "spurious-sqs": [{
+      Host: process.env["SQS.SPURIOS.LOCALHOST_NAME"].split("/").pop(),
+      HostPort: url.parse(process.env["SQS.SPURIOUS.LOCALHOST_PORT"]).port
+    }],
+    "spurious-s3": [{
+      Host: process.env["S3.SPURIOS.LOCALHOST_NAME"].split("/").pop(),
+      HostPort: url.parse(process.env["S3.SPURIOUS.LOCALHOST_PORT"]).port
+    }]
+  };
 }
 
 function config(type) {
-	switch (type) {
-		case "cli":
-			return port_config();
-			break;
-		case "docker":
-			return docker_config();
-			break;
-		default:
-		// throw err?
-	}
+  switch (type) {
+    case "cli":
+      return port_config();
+      break;
+    case "docker":
+      return docker_config();
+      break;
+    default:
+    // throw err?
+  }
 }
 
 function configure(_x, strategy) {
-	var type = arguments[0] === undefined ? "cli" : arguments[0];
-	strategy = strategy || new Strategy(true);
-	strategy.apply(config(type));
+  var type = arguments[0] === undefined ? "cli" : arguments[0];
+  strategy = strategy || new Strategy(true);
+  strategy.apply(config(type));
 }
 
-configure();
+// configure();
--- dist/strategy.js
+++ dist/strategy.js
@@ -5,87 +5,87 @@ var _prototypeProperties = function (child, staticProps, instanceProps) { if (st
 var AWS = require("aws-sdk");
 
 var Strategy = (function () {
-	function Strategy() {
-		var set_all = arguments[0] === undefined ? false : arguments[0];
-		this.mapping = {};
-		if (set_all) {
-			this.dynamo();
-			this.sqs();
-			this.s3();
-		}
-	}
+  function Strategy() {
+    var set_all = arguments[0] === undefined ? false : arguments[0];
+    this.mapping = {};
+    if (set_all) {
+      this.dynamo();
+      this.sqs();
+      this.s3();
+    }
+  }
 
-	_prototypeProperties(Strategy, null, {
-		dynamo: {
-			value: function dynamo() {
-				var port = arguments[0] === undefined ? true : arguments[0];
-				var ip = arguments[1] === undefined ? true : arguments[1];
-				this.mapping["spurious-dynamo"] = {
-					port: port,
-					ip: ip,
-					identifier: "dynamo_db"
-				};
-			},
-			writable: true,
-			configurable: true
-		},
-		sqs: {
-			value: function sqs() {
-				var port = arguments[0] === undefined ? true : arguments[0];
-				var ip = arguments[1] === undefined ? true : arguments[1];
-				this.mapping["spurious-sqs"] = {
-					port: port,
-					ip: ip,
-					identifier: "sqs"
-				};
-			},
-			writable: true,
-			configurable: true
-		},
-		s3: {
-			value: function s3() {
-				var port = arguments[0] === undefined ? true : arguments[0];
-				var ip = arguments[1] === undefined ? true : arguments[1];
-				this.mapping["spurious-s3"] = {
-					port: port,
-					ip: ip,
-					identifier: "s3"
-				};
-			},
-			writable: true,
-			configurable: true
-		},
-		apply: {
-			value: function apply(config) {
-				var _this = this;
-				// console.log(JSON.parse(config))
-				// config = JSON.parse(config)
-				Object.keys(this.mapping).forEach(function (key, idx, arr) {
-					console.log(key);
-					var ports = config[key];
-					console.log(ports);
-					console.log(_this.mapping);
-					if (_this.mapping[key].port != null) {
-						var a = {};
-						a["" + _this.mapping[key].identifier + "_port"] = ports[0].HostPort;
-						console.log(a);
-						new AWS.Config(a);
-					}
-					if (_this.mapping[key].ip != null) {
-						var a = {};
-						a["" + _this.mapping[key].identifier + "_endpoint"] = ports[0].Host;
-						console.log(a);
-						new AWS.Config(a);
-					}
-				});
-				// console.log(AWS.config)
-			},
-			writable: true,
-			configurable: true
-		}
-	});
+  _prototypeProperties(Strategy, null, {
+    dynamo: {
+      value: function dynamo() {
+        var port = arguments[0] === undefined ? true : arguments[0];
+        var ip = arguments[1] === undefined ? true : arguments[1];
+        this.mapping["spurious-dynamo"] = {
+          port: port,
+          ip: ip,
+          identifier: "dynamo_db"
+        };
+      },
+      writable: true,
+      configurable: true
+    },
+    sqs: {
+      value: function sqs() {
+        var port = arguments[0] === undefined ? true : arguments[0];
+        var ip = arguments[1] === undefined ? true : arguments[1];
+        this.mapping["spurious-sqs"] = {
+          port: port,
+          ip: ip,
+          identifier: "sqs"
+        };
+      },
+      writable: true,
+      configurable: true
+    },
+    s3: {
+      value: function s3() {
+        var port = arguments[0] === undefined ? true : arguments[0];
+        var ip = arguments[1] === undefined ? true : arguments[1];
+        this.mapping["spurious-s3"] = {
+          port: port,
+          ip: ip,
+          identifier: "s3"
+        };
+      },
+      writable: true,
+      configurable: true
+    },
+    apply: {
+      value: function apply(config) {
+        var _this = this;
+        // console.log(JSON.parse(config))
+        // config = JSON.parse(config)
+        Object.keys(this.mapping).forEach(function (key, idx, arr) {
+          // console.log(key)
+          var ports = config[key];
+          // console.log(ports);
+          // console.log(this.mapping)
+          if (_this.mapping[key].port != null) {
+            var a = {};
+            a["" + _this.mapping[key].identifier + "_port"] = ports[0].HostPort;
+            // console.log(a)
+            new AWS.Config(a);
+          }
+          if (_this.mapping[key].ip != null) {
+            var a = {};
+            a["" + _this.mapping[key].identifier + "_endpoint"] = ports[0].Host;
+            // console.log(a)
+            new AWS.Config(a);
+          }
+        });
+        // console.log(AWS.config)
+      },
+      writable: true,
+      configurable: true
+    }
+  });
 
-	return Strategy;
+  return Strategy;
 })();
 
 module.exports = Strategy;
