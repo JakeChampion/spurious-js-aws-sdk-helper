fba171eb0a154194dd056b9e84f3d60357400654
Jake Champion <jakechampion.jake2@googlemail.com>
2015-02-02 16:26:23 +0000

    adhere to style guide

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,8 @@
-var gulp = require('gulp');
-var to5 = require('gulp-6to5');
+var gulp = require('gulp')
+var to5 = require('gulp-6to5')
 
 gulp.task('default', function () {
-    return gulp.src('./src/*.js')
-        .pipe(to5())
-        .pipe(gulp.dest('dist'));
-});
+  return gulp.src('./src/*.js')
+  .pipe(to5())
+  .pipe(gulp.dest('dist'))
+})
--- src/strategy.js
+++ src/strategy.js
@@ -1,63 +1,63 @@
-var AWS = require("aws-sdk");
+let AWS = require("aws-sdk");
 
 class Strategy {
-	constructor(set_all = false) {
-		this.mapping = {};
-		if (set_all) {
-			this.dynamo()
-			this.sqs()
-			this.s3()
-		}
-	}
+  constructor(set_all = false) {
+    this.mapping = {};
+    if (set_all) {
+      this.dynamo()
+      this.sqs()
+      this.s3()
+    }
+  }
 
-	dynamo(port = true, ip = true) {
-		this.mapping['spurious-dynamo'] = {
-			port: port,
-			ip: ip,
-			identifier: 'dynamo_db'
-		}
-	}
+  dynamo(port = true, ip = true) {
+    this.mapping['spurious-dynamo'] = {
+      port: port,
+      ip: ip,
+      identifier: 'dynamo_db'
+    }
+  }
 
-	sqs(port = true, ip = true) {
-		this.mapping['spurious-sqs'] = {
-			port: port,
-			ip: ip,
-			identifier: 'sqs'
-		}
-	}
+  sqs(port = true, ip = true) {
+    this.mapping['spurious-sqs'] = {
+      port: port,
+      ip: ip,
+      identifier: 'sqs'
+    }
+  }
 
-	s3(port = true, ip = true) {
-		this.mapping['spurious-s3'] = {
-			port: port,
-			ip: ip,
-			identifier: 's3'
-		}
-	}
+  s3(port = true, ip = true) {
+    this.mapping['spurious-s3'] = {
+      port: port,
+      ip: ip,
+      identifier: 's3'
+    }
+  }
 
-	apply(config) {
-		// console.log(JSON.parse(config))
-		// config = JSON.parse(config)
-		Object.keys(this.mapping).forEach((key, idx, arr) => {
-			// console.log(key)
-			let ports = config[key];
-			// console.log(ports);
-			// console.log(this.mapping)
-			if (this.mapping[key]['port'] != null) {
-				let a = {};
-				a[`${this.mapping[key].identifier}_port`] = ports[0]['HostPort'];
-				// console.log(a)
-				new AWS.Config(a)
-			}
-            if (this.mapping[key]['ip'] != null) {
-            	let a = {};
-				a[`${this.mapping[key].identifier}_endpoint`] = ports[0]['Host'];
-				// console.log(a)
-				new AWS.Config(a)
-            	// AWS.config({`${mappings.identifier}_endpoint`: ports[0]['Host']})
-            }
-		});
-		// console.log(AWS.config)
-	}
+  apply(config) {
+    // console.log(JSON.parse(config))
+    // config = JSON.parse(config)
+    Object.keys(this.mapping).forEach((key, idx, arr) => {
+      // console.log(key)
+      let ports = config[key];
+      // console.log(ports);
+      // console.log(this.mapping)
+      if (this.mapping[key]['port'] != null) {
+        let a = {};
+        a[`${this.mapping[key].identifier}_port`] = ports[0]['HostPort'];
+        // console.log(a)
+        new AWS.Config(a)
+      }
+      if (this.mapping[key]['ip'] != null) {
+        let a = {};
+        a[`${this.mapping[key].identifier}_endpoint`] = ports[0]['Host'];
+        // console.log(a)
+        new AWS.Config(a)
+        // AWS.config({`${mappings.identifier}_endpoint`: ports[0]['Host']})
+      }
+    });
+    // console.log(AWS.config)
+  }
 }
 
 module.exports = Strategy;
