2ad0a304ab8b46fa621ed9e55829327db8a4b19a
Jake Champion <jakechampion.jake2@googlemail.com>
2015-02-22 22:29:03 +0000

    Switch mapping value to strings

--- dist/strategy.js
+++ dist/strategy.js
@@ -2,11 +2,15 @@
 
 var _prototypeProperties = function (child, staticProps, instanceProps) { if (staticProps) Object.defineProperties(child, staticProps); if (instanceProps) Object.defineProperties(child.prototype, instanceProps); };
 
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
 var AWS = require("aws-sdk");
 
 var Strategy = (function () {
   function Strategy() {
     var set_all = arguments[0] === undefined ? false : arguments[0];
+    _classCallCheck(this, Strategy);
+
     this.mapping = Object.create(null);
     if (set_all) {
       this.dynamo();
@@ -18,27 +22,21 @@ var Strategy = (function () {
   _prototypeProperties(Strategy, null, {
     dynamo: {
       value: function dynamo() {
-        this.mapping["spurious-dynamo"] = {
-          identifier: "dynamodb"
-        };
+        this.mapping["spurious-dynamo"] = "dynamodb";
       },
       writable: true,
       configurable: true
     },
     sqs: {
       value: function sqs() {
-        this.mapping["spurious-sqs"] = {
-          identifier: "sqs"
-        };
+        this.mapping["spurious-sqs"] = "sqs";
       },
       writable: true,
       configurable: true
     },
     s3: {
       value: function s3() {
-        this.mapping["spurious-s3"] = {
-          identifier: "s3"
-        };
+        this.mapping["spurious-s3"] = "s3";
       },
       writable: true,
       configurable: true
@@ -47,7 +45,7 @@ var Strategy = (function () {
       value: function apply(config) {
         for (var key in this.mapping) {
           var ports = config[key];
-          AWS.config[this.mapping[key].identifier] = {
+          AWS.config[this.mapping[key]] = {
             endpoint: ports[0].Host + ":" + ports[0].HostPort
           };
         }
--- src/strategy.js
+++ src/strategy.js
@@ -11,27 +11,21 @@ class Strategy {
   }
 
   dynamo() {
-    this.mapping['spurious-dynamo'] = {
-      identifier: 'dynamodb'
-    }
+    this.mapping['spurious-dynamo'] = 'dynamodb'
   }
 
   sqs() {
-    this.mapping['spurious-sqs'] = {
-      identifier: 'sqs'
-    }
+    this.mapping['spurious-sqs'] = 'sqs'
   }
 
   s3() {
-    this.mapping['spurious-s3'] = {
-      identifier: 's3'
-    }
+    this.mapping['spurious-s3'] = 's3'
   }
 
   apply(config) {
     for (let key in this.mapping) {
       let ports = config[key];
-      AWS.config[this.mapping[key].identifier] = {
+      AWS.config[this.mapping[key]] = {
         endpoint: ports[0]['Host'] + ":" + ports[0]["HostPort"]
       }
     }
