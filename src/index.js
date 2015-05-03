7623ba3456f1dbe252a1425a7d3f80e699aa7bb8
Jake Champion <jakechampion.jake2@googlemail.com>
2015-05-03 17:54:52 +0100

    adhere to styleguide

--- src/index.js
+++ src/index.js
@@ -1,63 +1,63 @@
 const url = require('url')
 const Strategy = require('./strategy')
 
-function get_ports() {
+function get_ports () {
   //  Use execSync if it exist (Node 0.12 and later) or exec-sync
-  const execSync = require('child_process').execSync || require('exec-sync');
-  const ports = execSync('spurious ports --json');
+  const execSync = require('child_process').execSync || require('exec-sync')
+  const ports = execSync('spurious ports --json')
   if (typeof ports === 'string') {
-    return ports;
+    return ports
   }
 
-  const Buffer = require('buffer').Buffer;
+  const Buffer = require('buffer').Buffer
   if (Buffer.isBuffer(ports)) {
-    return ports.toString();
+    return ports.toString()
   }
-  
-  throw new Error(ports);
+
+  throw new Error(ports)
 }
 
 function port_config () {
-  const ports = get_ports();
-  if (ports == "[error] Spurious services haven't been started, please run 'spurious start'") {
-    throw new Error(ports);
+  const ports = get_ports()
+  if (ports === '[error] Spurious services haven\'t been started, please run \'spurious start\'') {
+    throw new Error(ports)
   }
-  return JSON.parse(ports);
+  return JSON.parse(ports)
 }
 
-function docker_config() {
+function docker_config () {
   return {
-    "spurious-dynamo": [
+    'spurious-dynamo': [
       {
-        Host: process.env["DYNAMODB.SPURIOUS.LOCALHOST_NAME"].split("/").pop(),
-        HostPort: url.parse(process.env["DYNAMODB.SPURIOUS.LOCALHOST_PORT"]).port
+        Host: process.env['DYNAMODB.SPURIOUS.LOCALHOST_NAME'].split('/').pop(),
+        HostPort: url.parse(process.env['DYNAMODB.SPURIOUS.LOCALHOST_PORT']).port
       }
     ],
-    "spurious-sqs": [
+    'spurious-sqs': [
       {
-        Host: process.env["SQS.SPURIOUS.LOCALHOST_NAME"].split("/").pop(),
-        HostPort: url.parse(process.env["SQS.SPURIOUS.LOCALHOST_PORT"]).port
+        Host: process.env['SQS.SPURIOUS.LOCALHOST_NAME'].split('/').pop(),
+        HostPort: url.parse(process.env['SQS.SPURIOUS.LOCALHOST_PORT']).port
       }
     ],
-    "spurious-s3": [
+    'spurious-s3': [
       {
-        Host: process.env["S3.SPURIOUS.LOCALHOST_NAME"].split("/").pop(),
-        HostPort: url.parse(process.env["S3.SPURIOUS.LOCALHOST_PORT"]).port
+        Host: process.env['S3.SPURIOUS.LOCALHOST_NAME'].split('/').pop(),
+        HostPort: url.parse(process.env['S3.SPURIOUS.LOCALHOST_PORT']).port
       }
     ]
-  };
+  }
 }
 
-function config(type) {
-  switch(type) {
-    case "docker":
+function config (type) {
+  switch (type) {
+    case 'docker':
       return docker_config()
     default:
       return port_config()
   }
 }
 
-function configure(type, strategy) {
+function configure (type, strategy) {
   strategy = strategy || new Strategy(true)
   strategy.apply(config(type))
 }
--- src/strategy.js
+++ src/strategy.js
@@ -1,8 +1,8 @@
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk')
 
 class Strategy {
-  constructor(set_all = false) {
-    this.mapping = Object.create(null);
+  constructor (set_all = false) {
+    this.mapping = Object.create(null)
     if (set_all) {
       this.dynamo()
       this.sqs()
@@ -10,26 +10,26 @@ class Strategy {
     }
   }
 
-  dynamo() {
+  dynamo () {
     this.mapping['spurious-dynamo'] = 'dynamodb'
   }
 
-  sqs() {
+  sqs () {
     this.mapping['spurious-sqs'] = 'sqs'
   }
 
-  s3() {
+  s3 () {
     this.mapping['spurious-s3'] = 's3'
   }
 
-  apply(config) {
+  apply (config) {
     for (let key in this.mapping) {
-      let ports = config[key];
+      let ports = config[key]
       AWS.config[this.mapping[key]] = {
-        endpoint: ports[0]['Host'] + ":" + ports[0]["HostPort"]
+        endpoint: ports[0]['Host'] + ':' + ports[0]['HostPort']
       }
     }
   }
 }
 
-module.exports = Strategy;
+module.exports = Strategy
